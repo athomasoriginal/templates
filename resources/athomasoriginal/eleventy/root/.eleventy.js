b130b6c940cc04ce12bbfb8f535689a3e7ec04e2
athomasoriginal <thomasmattacchione@gmail.com>
2023-11-10 18:38:51 -0500

    enhance: add template - `eleventy`

---
+++ resources/athomasoriginal/eleventy/root/.eleventy.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+const filter = require("./src/filter");
+
+// @note import plugins
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function (eleventyConfig) {
+  // @configuration avoid using .gitignore to tell eleventy what should/should
+  // not be watched
+  eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
+
+  eleventyConfig.addFilter("dateFilter", filter.dateFilter);
+
+  eleventyConfig.addFilter("w3cDate", filter.w3cDate);
+
+  // @configuration rock an RSS feed
+  eleventyConfig.addPlugin(pluginRss);
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+    passthroughFileCopy: true,
+  };
+};
